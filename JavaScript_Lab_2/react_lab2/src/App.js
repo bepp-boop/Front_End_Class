2c01d1f665a0727a4dfd42f3bef1e8c199990221
Quang M Nguyen <quangteoqs15@gmail.com>
2022-03-11 09:35:23 +0100

    Trying my best

--- JavaScript_Lab_2/react_lab2/src/App.js
+++ JavaScript_Lab_2/react_lab2/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import User from './components/User'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.pageTitle = "Event handlers";
+
+    this.state = {
+      quiz: {
+        php: false,
+        java: false,
+        python: false,
+      }
+    }
+  }
+
+  handleCheckbox = data => {
+    let state = this.state;
+    state.quiz[data.target.value] = data.target.checked;
+    this.setState(state);
+
+    console.log(this.state.quiz)
+  }
+
+  handleScore = () => {
+    let state = this.state;
+    let score = 0;
+    {...this.state.quiz }
+  }
+    console.log(score)
+
 }
 
-export default App;
+render() {
+  return (
+    < div className="container" >
+      <Header title="Quize"></Header>
+      <User></User>
+      <div className="quiz">
+        php<input onChange={this.handleCheckbox} type="checkbox" name="quiz" value="php" checked={this.state.quiz.php} />
+        java<input onChange={this.handleCheckbox} type="checkbox" name="quiz" value="java" checked={this.state.quiz.jaba} />
+        python<input onChange={this.handleCheckbox} type="checkbox" name="quiz" value="python" checked={this.state.quiz.python} />
+      </div>
+      <input onClick={this.handleScore} type="button" value="Score" />
+    </div>
+  )
+}
+}
+
+export default App
